1f2edae35c01ebb1fb2ff54f6c6f4c683b4a5ca4
A00198515 <kacpi.borko@gmail.com>
2015-10-19 15:26:15 +0200

    Update first-server.js

--- first-server.js
+++ first-server.js
@@ -29,6 +29,7 @@ var consolere = require('console-remote-client').connect('console.re','80','a001
 function handleRequest(request, response){
     response.end('Yes! It Works!! Path Hit: ' + server_ip_address + ":" + server_port + "/" + request.url);
     console.re.log("My server ip address is: " + server_ip_address);
+    console.re.log("My server port is: " + server_port);
 }
 //What is a request?
 //What is a response?
